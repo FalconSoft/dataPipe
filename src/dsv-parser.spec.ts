855b08325e0caf70f29847937b60d1fa93bdbb38
Pavlo Paska <ppaska@gmail.com>
2020-02-11 18:46:31 +0000

    added DSV parser functionality

--- src/dsv-parser.spec.ts
+++ src/dsv-parser.spec.ts
@@ -3,23 +3,23 @@ import { fromCsv, ParsingOptions } from './dsv-parser';
 
 describe('Dsv Parser specification', () => {
   it('simple numbers', () => {
-    const csv = ["F1,F2","1,2"].join('\n')
-    const result =  fromCsv(csv);
+    const csv = ["F1,F2", "1,2"].join('\n')
+    const result = fromCsv(csv);
     expect(result.length).toBe(1);
     expect(result[0].F1).toBe(1);
     expect(result[0].F2).toBe(2);
   })
 
-  it('simple numders and strings', () => {    
-    const csv = ["F1,F2,F3",`1,2,"Test, comma"`].join('\n')
-    const result =  fromCsv(csv);
+  it('simple numders and strings', () => {
+    const csv = ["F1,F2,F3", `1,2,"Test, comma"`].join('\n')
+    const result = fromCsv(csv);
     expect(result.length).toBe(1);
     expect(result[0].F3).toBe('Test, comma');
   })
 
-  it('Empty should be null', () => {    
-    const csv = ["F1,F2,F3",`1,,"Test, comma"`].join('\n')
-    const result =  fromCsv(csv);
+  it('Empty should be null', () => {
+    const csv = ["F1,F2,F3", `1,,"Test, comma"`].join('\n')
+    const result = fromCsv(csv);
     expect(result.length).toBe(1);
     expect(result[0].F2).toBe(null);
   })
@@ -28,27 +28,73 @@ describe('Dsv Parser specification', () => {
     const multiLineString = `this is , 5 - ,
     multi-line
     string`;
-    const csv = ["F1,F2,F3",`1,"${multiLineString}","Test, comma"`].join('\n')
-    const result =  fromCsv(csv);
+    const csv = ["F1,F2,F3", `1,"${multiLineString}","Test, comma"`].join('\n')
+    const result = fromCsv(csv);
     expect(result.length).toBe(1);
     expect(result[0].F2).toBe(multiLineString);
-  })  
+  })
 
   it('Multiline string DSV', () => {
     const multiLineString = `this is , 5 - ,
     multi-line
     string`;
-    const csv = ["F1\tF2\tF3",`1\t"${multiLineString}"\t"Test, comma"`].join('\n')
-    const result =  fromCsv(csv, <ParsingOptions>{separator:'\t'});
+    const csv = ["F1\tF2\tF3", `1\t"${multiLineString}"\t"Test, comma"`].join('\n')
+    const result = fromCsv(csv, <ParsingOptions>{ separator: '\t' });
     expect(result.length).toBe(1);
     expect(result[0].F2).toBe(multiLineString);
-  })  
+  })
 
   it('DSV with comma numbers', () => {
-    const csv = ["F1\tF2\tF3",`1\t1,000.32\t"Test, comma"`].join('\n')
-    const result =  fromCsv(csv, <ParsingOptions>{separator:'\t'});
+    const csv = ["F1\tF2\tF3", `1\t1,000.32\t"Test, comma"`].join('\n')
+    const result = fromCsv(csv, <ParsingOptions>{ separator: '\t' });
     expect(result.length).toBe(1);
     expect(result[0].F2).toBe(1000.32);
-  })  
+  })
+
+  it('skip rows', () => {
+    const csv = ["", "", "F1\tF2\tF3", `1\t1,000.32\t"Test, comma"`].join('\n')
+    const result = fromCsv(csv, <ParsingOptions>{ separator: '\t', skipRows: 2 });
+    expect(result.length).toBe(1);
+    expect(result[0].F2).toBe(1000.32);
+  })
+  it('skip rows not empty rows', () => {
+    const csv = ["", " * not Empty *", "F1\tF2\tF3", `1\t1,000.32\t"Test, comma"`].join('\n')
+    const result = fromCsv(csv, <ParsingOptions>{ separator: '\t', skipRows: 2 });
+    expect(result.length).toBe(1);
+    expect(result[0].F2).toBe(1000.32);
+  })
+
+  it('skipUntil', () => {
+    const csv = ["", " * not Empty *", "F1\tF2\tF3", `1\t1,000.32\t"Test, comma"`].join('\n')
+    var options = new ParsingOptions();
+    options.separator = '\t';
+    options.skipUntil = t => t && t.length > 1;
+
+    const result = fromCsv(csv, options);
+    expect(result.length).toBe(1);
+    expect(result[0].F2).toBe(1000.32);
+  })
+
+  it('empty values', () => {
+    const csv = ["", "",, "\t\t\t", "F1\tF2\tF3", `1\t1,000.32\t"Test, comma"`, "\t\t"].join('\n')
+    var options = new ParsingOptions();
+    options.separator = '\t';
+    
+    const result = fromCsv(csv, options);
+    expect(result.length).toBe(1);
+    expect(result[0].F2).toBe(1000.32);
+  })
+
+  it('Date Fields', () => {
+    const csv = ["F1\tF2\tF3", `2020-02-11\t1,000.32\t"Test, comma"`].join('\n')
+    var options = new ParsingOptions();
+    options.separator = '\t';
+    options.dateFields = ['F1']
+    
+    const result = fromCsv(csv, options);
+    expect(result.length).toBe(1);
+    expect(result[0].F1 instanceof Date).toBe(true);
+  })
+
 
 })
--- src/dsv-parser.ts
+++ src/dsv-parser.ts
@@ -1,25 +1,41 @@
-import { parseNumberOrNull } from "./utils";
+import { parseNumberOrNull, parseDatetimeOrNull } from "./utils";
 
 export class ParsingOptions {
     separator: string = ',';
     skipRows: number = 0;
+    dateFields: string[] = [];
+    numberFields: string[] = [];
+    booleanFields: string[] = [];
     skipUntil?: (tokens: string[]) => boolean;
     takeWhile?: (tokens: string[]) => boolean;
     parseFields?: {};
     elementSelector?: (headers: string[], tokens: string[]) => any;
 }
 
-type ParsingContext = { content: string, currentIndex: number };
+type ParsingContext = {
+    content: string,
+    currentIndex: number
+};
 
-function getObjectElement(fieldNames: string[], tokens: string[]): any {
+function getObjectElement(fieldNames: string[], tokens: string[], options: ParsingOptions): any {
     const obj = Object.create(null);
     for (let i = 0; i < fieldNames.length; i++) {
-        let value: string | number | null = tokens[i] || null;
+        const fieldName = fieldNames[i];
+        let value: string | number | Date | boolean | null = tokens[i] || null;
+
         if (value && value.length) {
-            const num = parseNumberOrNull(value);
-            value = num || value;
+            if (options.dateFields && options.dateFields.indexOf(fieldName) >= 0) {
+                value = parseDatetimeOrNull(value);
+            } else if (options.numberFields && options.numberFields.indexOf(fieldName) >= 0) {
+                value = parseNumberOrNull(value);
+            } else if (options.booleanFields && options.booleanFields.indexOf(fieldName) >= 0) {                
+                value = !!value;
+            } else {
+                const num = parseNumberOrNull(<string>value);
+                value = num || value;
+            }
         }
-        obj[fieldNames[i]] = value
+        obj[fieldName] = value
     }
     return obj;
 }
@@ -29,25 +45,56 @@ function getLineTokens(content: string, options: ParsingOptions): string[][] {
         content: content,
         currentIndex: 0
     };
-    content = (content || '').trim();
+    content = content || '';
     const delimiter = options.separator || ',';
 
     const result = [];
-    let line = 0;
+    let lineNumber = 0;
     let fieldNames: string[] | null = null;
+    let isEmpty = true;
 
     do {
         const tokens = nextLineTokens(ctx, delimiter);
-        if (!fieldNames) {
-            fieldNames = tokens;
+
+        isEmpty = tokens.filter(f => !f || !f.length).length === tokens.length;
+
+        if (isEmpty) {
+            lineNumber++;
             continue;
+        }
+
+        // skip rows based skipRows value
+        if (lineNumber < options.skipRows) {
+            lineNumber++;
+            continue;
+        }
+
+        // skip rows based on skipUntil call back
+        if (!fieldNames && typeof options.skipUntil === "function" && !options.skipUntil(tokens)) {
+            lineNumber++;
+            continue;
+        }
+
+        if (!fieldNames) {
+            // fieldName is used as indicator on whether data rows handling started
+            fieldNames = tokens;
+            lineNumber++;
+            continue;
+        }
+
+        if (typeof options.takeWhile === "function" && fieldNames && !options.takeWhile(tokens)) {
+            break;
         }
 
         const obj = (typeof options.elementSelector === "function") ?
             options.elementSelector(fieldNames, tokens)
-            : getObjectElement(fieldNames, tokens)
+            : getObjectElement(fieldNames, tokens, options)
 
-        result.push(obj);
+        if (obj) {
+            // no need for null or empty objects
+            result.push(obj);
+        }
+        lineNumber++;
     }
     while (++ctx.currentIndex < ctx.content.length)
 
@@ -56,8 +103,7 @@ function getLineTokens(content: string, options: ParsingOptions): string[][] {
 
 export function fromCsv(content: string, options?: ParsingOptions): any[] {
     const result: any[] = [];
-    content = (content || '').trim();
-    const delimiter = options?.separator || ',';
+    content = content || '';
 
     if (!content.length) {
         return result;
